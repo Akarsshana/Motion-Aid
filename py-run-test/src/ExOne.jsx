7d22e2b4ed0d07993aab47e97c64edd65ef42280
Akarsshana <152950274+Akarsshana@users.noreply.github.com>
2025-03-27 23:01:20 +0530

    Updated files

---
+++ py-run-test/src/ExOne.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExOne() {
+  return (
+    <div style={{ textAlign: "center", padding: "490px", background: "linear-gradient(to top, #3D0075,#000000)", height: "0vh", color: "white" }}>
+      <h1>Welcome to ExOne Page</h1>
+    </div>
+  );
+}
+
+export default ExOne;
--- py-run-test/src/main.jsx
+++ py-run-test/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Added missing imports
+import './index.css';
+import App from './App.jsx';
+import ExOne from './ExOne.jsx';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> {/* Fixed Router issue */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/exone" element={<ExOne/>}/>
+        
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
